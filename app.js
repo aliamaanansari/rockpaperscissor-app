ee7d22d62dab8715a2a7645210d3d0c3d3eff4ef
Ali Aman <aliamaan071@gmail.com>
2023-10-31 12:30:54 +0530

    added reset and score counter

--- app.js
+++ app.js
@@ -1,34 +1,37 @@
 const totalScore = { computerScore: 0, playerScore: 0 };
+const op = {
+  ROCK: "ROCK",
+  PAPER: "PAPER",
+  SCISSOR: "SCISSOR",
+};
 
 function getComputerChoice() {
-  const rpsChoice = ["Rock", "Paper", "Scissors"];
+  const rpsChoice = [op.ROCK, op.PAPER, op.SCISSOR];
   const randomNumber = Math.floor(Math.random() * 3);
   return rpsChoice[randomNumber];
 }
-console.log(getComputerChoice());
 
 function getResult(playerChoice, computerChoice) {
   let score;
   if (playerChoice == computerChoice) {
     score = 0;
-  } else if (playerChoice == "Rock" && computerChoice == "Scissors") {
+  } else if (playerChoice == op.ROCK && computerChoice == op.SCISSOR) {
     score = 1;
-  } else if (playerChoice == "Paper" && computerChoice == "Rock") {
+  } else if (playerChoice == op.PAPER && computerChoice == op.ROCK) {
     score = 1;
-  } else if (playerChoice == "Scissors" && computerChoice == "Paper") {
+  } else if (playerChoice == op.SCISSOR && computerChoice == op.PAPER) {
     score = 1;
   } else {
     score = -1;
   }
-
   return score;
 }
 
-// ** showResult updates the DOM to `You Win!` or `You Lose!` or `It's a Draw!` based on the score. Also shows Player Choice vs. Computer Choice**
 function showResult(score, playerChoice, computerChoice) {
   const resultDiv = document.getElementById("result");
   const handsDiv = document.getElementById("hands");
   const playerScoreDiv = document.getElementById("player-score");
+  const computerScoreDiv = document.getElementById("computer-score");
   if (score == -1) {
     resultDiv.innerHTML = "You Lose!!!!!! ";
   } else if (score == 0) {
@@ -36,32 +39,47 @@ function showResult(score, playerChoice, computerChoice) {
   } else {
     resultDiv.innerHTML = "You Won!!!!!!!!";
   }
-  handsDiv.innerText = `Player = ${playerChoice} vs Computer = ${computerChoice}`;
+  handsDiv.innerText = `Your = ${playerChoice} vs Computer = ${computerChoice}`;
   playerScoreDiv.innerText = `Your Score: ${totalScore["playerScore"]}`;
+  computerScoreDiv.innerText = `Computer Score: ${totalScore["computerScore"]}`;
 }
 
-// ** Calculate who won and show it on the screen **
 function onClickRPS(playerChoice) {
-  console.log(playerChoice);
   const computerChoice = getComputerChoice();
-
-  console.log({ computerChoice });
   const score = getResult(playerChoice, computerChoice);
-  totalScore["playerScore"] += score;
   console.log({ score });
+  if (score) {
+    totalScore["playerScore"] += score;
+    totalScore["computerScore"] -= score;
+  } else {
+    totalScore["playerScore"] -= score;
+    totalScore["computerScore"] += score;
+  }
   showResult(score, playerChoice, computerChoice);
 }
 
-// ** Make the RPS buttons actively listen for a click and do something once a click is detected **
 function playGame() {
-  // use querySelector to select all RPS Buttons
   const rpsButtons = document.querySelectorAll(".rpsButton");
   rpsButtons.forEach((rpsButtons) => {
     rpsButtons.onclick = () => onClickRPS(rpsButtons.value);
   });
+  const endGameButton = document.getElementById("endGameButton");
+  endGameButton.onclick = () => endGame(totalScore);
 }
 
-// ** endGame function clears all the text on the DOM **
-function endGame() {}
+function endGame(totalScore) {
+  totalScore["playerScore"] = 0;
+  totalScore["computerScore"] = 0;
+
+  const resultDiv = document.getElementById("result");
+  const handsDiv = document.getElementById("hands");
+  const playerScoreDiv = document.getElementById("player-score");
+  const computerScoreDiv = document.getElementById("computer-score");
+
+  resultDiv.innerText = "";
+  handsDiv.innerText = "";
+  playerScoreDiv.innerText = "";
+  computerScoreDiv.innerText = "";
+}
 
 playGame();
